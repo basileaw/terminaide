7928f0f4e14ed8858e019601bbf1ccb285137741
Alex Basile <basileaw@gmail.com>
2025-03-13 08:02:39 -0700

    remove instructions

--- terminaide/demos/__init__.py
+++ terminaide/demos/__init__.py
@@ -10,7 +10,7 @@ Example:
     from terminaide import demos
     
     if __name__ == "__main__":
-        # Run the default demo (snake)
+        # Run the default demo (index)
         demos.run()
         
         # Or explicitly choose a demo
@@ -18,17 +18,16 @@ Example:
         demos.play_snake()
         demos.play_pong()
         demos.play_tetris()
-        demos.show_instructions()
 """
 
 from .snake import run_demo as _run_snake
 from .pong import run_demo as _run_pong
 from .tetris import run_demo as _run_tetris
-from .instructions import run_demo as _show_instructions
 from .index import run_demo as _show_index
 
 def run():
-    _show_instructions()
+    # Change default demo to show the index of available demos
+    _show_index()
 
 def play_snake():
     _run_snake()
@@ -39,11 +38,8 @@ def play_pong():
 def play_tetris():
     _run_tetris()
 
-def show_instructions():
-    _show_instructions()
-
 def show_index():
     _show_index()
 
-demos=run
-__all__=["run","play_snake","play_pong","play_tetris","show_instructions","show_index","demos"]
+demos = run
+__all__ = ["run", "play_snake", "play_pong", "play_tetris", "show_index", "demos"]
