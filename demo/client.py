5d707daec6f092bc16b4fce22149c87598ab6eb1
Alex Basile <basileaw@gmail.com>
2025-06-03 16:57:33 -0700

    remove arg parsing

--- demo/client.py
+++ demo/client.py
@@ -4,31 +4,12 @@
 This script demonstrates how to use terminaide's built-in games.
 """
 
-import os
-import sys
 import argparse
-from terminaide.terminarcade import play_snake, play_pong, play_tetris, show_index
+from terminaide import terminarcade
 
 
 def main():
-    """Parse command-line arguments and run the selected game."""
-    parser = argparse.ArgumentParser(description="Terminaide games client")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--index", action="store_true", help="Show the games menu")
-    group.add_argument("--snake", action="store_true", help="Play Snake")
-    group.add_argument("--pong", action="store_true", help="Play Pong")
-    group.add_argument("--tetris", action="store_true", help="Play Tetris")
-
-    args = parser.parse_args()
-
-    if args.snake:
-        play_snake()
-    elif args.pong:
-        play_pong()
-    elif args.tetris:
-        play_tetris()
-    else:
-        show_index()
+    terminarcade("games")
 
 
 if __name__ == "__main__":
